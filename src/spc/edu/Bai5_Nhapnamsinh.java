6e41ad85b0159122571a78ef1dc268ab9128afd7
feldway1 <feldway120@gmail.com>
2024-09-10 11:16:05 +0700

    thêm vài bài nữa????

--- src/spc/edu/Bai5_Nhapnamsinh.java
+++ src/spc/edu/Bai5_Nhapnamsinh.java
@@ -11,12 +11,15 @@ import  java.util.Scanner;
 public class Bai5_Nhapnamsinh {
     public static void main(String[] args) {
         System.out.println("Chuong trinh tinh tuoi");
+        System.out.print("Nhap ho va ten:");
         Scanner sc = new Scanner(System.in);
+        String hoten = sc.nextLine();
         System.out.print("Nhap so nam sinh: ");
         int namsinh = sc.nextInt();
         System.out.print("Nhap so nam hien tai: ");
         int namhientai = sc.nextInt();
         int tuoi = namhientai - namsinh;
+        System.out.println("Chao ban "+ hoten);
         System.out.println("So tuoi la: "+ tuoi);
     }
 }
