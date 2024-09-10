72e7e0e22be439e9f69015fc7455ad4a66f7b229
feldway1 <Administrator@LAB1-MAY13>
2024-09-10 14:08:39 +0700

    thêm vài bài nữa???

---
+++ src/spc/edu/Bai6_Hinhchunhat.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import  java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai6_Hinhchunhat {
+    public static void main(String[] args) {
+        System.out.println("Hinh chu nhat");
+        double chieudai, chieurong, chuvi, dientich;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap chieu dai: ");
+        chieudai = sc.nextDouble();
+        System.out.print("Nhap chieu rong: ");
+        chieurong = sc.nextDouble();
+        chuvi = (chieudai + chieurong)*2;
+        System.out.println("Chu vi hinh chu nhat = " + chuvi);
+        dientich = chieudai * chieurong;
+        System.out.println("Dien tich hinh chu nhat = "+ dientich);
+    }
+}
